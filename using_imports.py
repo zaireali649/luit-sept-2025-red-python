a7c81bb41aeb87afbcf0278c549e81f89ddb3329
Zaire Ali <zaireali649@gmail.com>
2025-09-18 20:40:15 -0400

    Update using_imports.py

--- using_imports.py
+++ using_imports.py
@@ -1,6 +1,27 @@
+import os
+import sys
 import random
-# from random import randint
+import matplotlib.pyplot as plt
+import pyfiglet  # obscure: generates ASCII art text
+import hello_world
 
-number = random.randint(0, 10)
+# Show some OS info
+print("Current working directory:", os.getcwd())
+print("Python executable:", sys.executable)
 
-print(number)
+# Generate random numbers
+numbers = [random.randint(0, 10) for _ in range(10)]
+print("Random numbers:", numbers)
+
+# Plot with matplotlib
+plt.plot(numbers, marker="o", linestyle="-", color="purple")
+plt.title("Random Numbers Plot")
+plt.xlabel("Index")
+plt.ylabel("Value")
+plt.show()
+
+# Use obscure library: pyfiglet
+ascii_banner = pyfiglet.figlet_format("Python Rocks!")
+print(ascii_banner)
+
+hello_world.say_hello()
